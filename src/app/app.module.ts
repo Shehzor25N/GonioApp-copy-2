eba9c7932cce9da2bb589893dd067b54aae1c222
Shehzor25N <shehzor381@gmail.com>
2023-06-02 00:46:29 +0400

    Draft Gait App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+  import { NgModule } from '@angular/core';
+  import { BrowserModule } from '@angular/platform-browser';
+  import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+  import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { getDatabase, ref, push, set, onValue } from 'firebase/database';
-import { Chart, ChartOptions, ChartData } from 'chart.js';
-import { WelcomePageModule } from './welcome/welcome.module';
+  import { AppComponent } from './app.component';
+  import { AppRoutingModule } from './app-routing.module';
+  import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+  import { getDatabase, ref, push, set, onValue } from 'firebase/database';
+  import { Chart, ChartOptions, ChartData } from 'chart.js';
+  import { WelcomePageModule } from './welcome/welcome.module';
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "firebase/app";
+  import { getAnalytics } from "firebase/analytics";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAXYL-v3UV6zIAD0YbQwTBZqTgoxcjCve0",
-  authDomain: "controlandmonitorapp.firebaseapp.com",
-  projectId: "controlandmonitorapp",
-  storageBucket: "controlandmonitorapp.appspot.com",
-  messagingSenderId: "900507619738",
-  appId: "1:900507619738:web:77a7551416c7be1d81ea23",
-  measurementId: "G-YVE76J89WF"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAXYL-v3UV6zIAD0YbQwTBZqTgoxcjCve0",
+    authDomain: "controlandmonitorapp.firebaseapp.com",
+    projectId: "controlandmonitorapp",
+    storageBucket: "controlandmonitorapp.appspot.com",
+    messagingSenderId: "900507619738",
+    appId: "1:900507619738:web:77a7551416c7be1d81ea23",
+    measurementId: "G-YVE76J89WF"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
+  @NgModule({
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent],
+  })
+  export class AppModule {}
